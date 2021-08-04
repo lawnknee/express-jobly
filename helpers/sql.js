c6168c0a09bf57596274af2f6115509e3ea387e1
ZeeTom <zthom13@hotmail.com>
2021-08-04 13:09:50 -0600

    added filtering options to findAll method in companies models

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,23 +2,23 @@ const { BadRequestError } = require("../expressError");
 
 /** Takes in user data, such as:
  *    { firstName, lastName, password, email, isAdmin }
- *  
+ *
  *  Creates an array with the column names parameterized.
- * 
+ *
  *  Returns an object like:
- *    { 
+ *    {
  *      setCols: joins the array with comma-separated elements,
  *      values: array of values from dataToUpdate
- *    }  
- */ 
+ *    }
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,22 +1,30 @@
-"use strict"
+"use strict";
 
 const { sqlForPartialUpdate } = require("./sql");
-const { BadRequestError } = require("../expressError")
+const { BadRequestError } = require("../expressError");
 
+let jsToSql = {
+  firstName: "first_name",
+  lastName: "last_name",
+  isAdmin: "is_admin",
+};
 
 describe("sqlForPartialUpdate", function () {
   test("valid input: updating partial user data", function () {
-    let jsToSql = {
-      firstName: "first_name",
-      lastName: "last_name",
-      isAdmin: "is_admin",
-    };
-
-    expect(sqlForPartialUpdate({firstName: 'Aliya', age: 32}, jsToSql)).toEqual({
-      setCols: "\"first_name\"=$1, \"age\"=$2",
-      values: ['Aliya', 32]
+    expect(
+      sqlForPartialUpdate({ firstName: "Aliya", age: 32 }, jsToSql)
+    ).toEqual({
+      setCols: '"first_name"=$1, "age"=$2',
+      values: ["Aliya", 32],
     });
-  })
+  });
 
-  test("invalid")
-})
+  test("invalid input", function () {
+    try {
+      sqlForPartialUpdate({}, jsToSql);
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
