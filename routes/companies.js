41e68468a76c954e6c84a43e43d9a6f8ea49d8b6
Loni <kuangloni@gmail.com>
2021-08-04 12:19:51 -0700

    filter schema and get route updated with query validation

--- routes/companies.js
+++ routes/companies.js
@@ -11,6 +11,7 @@ const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const companyFilteringSchema = require("../schemas/companyFiltering.json");
 
 const router = new express.Router();
 
@@ -47,7 +48,15 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  */
 
 router.get("/", async function (req, res, next) {
-  const companies = await Company.findAll();
+  const query = req.query;
+  const result = jsonschema.validate(query, companyFilteringSchema)
+
+  if (!result.valid) {
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const companies = await Company.findAll(query);
   return res.json({ companies });
 });
 
