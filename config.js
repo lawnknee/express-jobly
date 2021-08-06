07a5a0ef98456e0c99fa5f65b1319f5d40278fff
Loni <kuangloni@gmail.com>
2021-08-06 11:11:05 -0700

    heroku changes

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
+// heroku gives us one called DATABASE_URL
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "jobly_test"
